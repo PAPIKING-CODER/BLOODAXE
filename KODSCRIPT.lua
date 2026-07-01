4dfeb4dae090f964f0817e4c3a8a59d1ce8c60c2
PAPIKING-CODER <waporhack@gmail.com>
2026-07-01 03:23:05 -0400

    Update script URL in KODSCRIPT.lua

--- KODSCRIPT.lua
+++ KODSCRIPT.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=4172644536325"))("4172644536325")
+loadstring(game:HttpGet("https://encrypt-x.pages.dev/Scripts?Id=9358283136384"))("9358283136384")
